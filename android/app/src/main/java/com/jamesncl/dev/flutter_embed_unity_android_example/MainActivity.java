2b23e2dd29c5666bfb21f1e1a53e70276f112428
Katarzyna-Hajduk <72875387+Ssilith@users.noreply.github.com>
2024-12-02 19:36:50 +0100

    Initial commit

---
+++ android/app/src/main/java/com/jamesncl/dev/flutter_embed_unity_android_example/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.ar_vis;
+
+import com.learntoflutter.flutter_embed_unity_android.unity.FakeUnityPlayerActivity;
+
+public class MainActivity extends FakeUnityPlayerActivity {
+
+}
